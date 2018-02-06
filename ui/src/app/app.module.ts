4ed5935e1d957b78e807d0f94e3a9a940e8281e7
Dorgaa Fahed <fahed.dorgaa@outscale.com>
2018-02-06 23:13:22 +0100

    project structure

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { RakingComponent } from './body/raking/raking.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RakingComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ui/src/app/body/raking/raking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raking',
+  templateUrl: './raking.component.html',
+  styleUrls: ['./raking.component.scss']
+})
+export class RakingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
